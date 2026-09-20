0b964076cfc0e0dd9c8cfc57ab3836aab4e6a1ae
PRIYANSHU GHOSH <priyanshughosh676@gmail.com>
2026-09-20 22:45:29 +0530

    Allow production frontend CORS

--- tourist_ai_model/api.py
+++ tourist_ai_model/api.py
@@ -41,15 +41,10 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-
-        # In case Vite uses another common development port
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000",
+    "https://atithibandhu-frontend.vercel.app",
     ],
 
     allow_credentials=True,
